4b9df8fda072850cb69070a379f819b803a3cacc
miso1105 <137920347+miso1105@users.noreply.github.com>
2023-12-11 09:33:59 +0900

    [level unrated] Title: 짝수의 합, Time: 0.32 ms, Memory: 60.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        for (i in 2..n step 2){
+            answer += i
+        }
+        return answer
+
+    }
+}
