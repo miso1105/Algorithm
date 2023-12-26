5990cf47439a59dd52078be4443583264cfac0b6
miso1105 <137920347+miso1105@users.noreply.github.com>
2023-12-26 11:00:25 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 12.93 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(n: Long): IntArray {
+        var answer = intArrayOf()
+        answer = n.toString().reversed().map { it.toString().toInt() }.toIntArray()
+        return answer
+    }
+}
