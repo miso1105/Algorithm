6bfde6fa293541682f80fb4d04b3e81bfc661bcb
miso1105 <137920347+miso1105@users.noreply.github.com>
2024-03-24 21:46:45 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.04 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -3,11 +3,12 @@ class Solution {
         var answer = 0
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        var nStr = n.toString().toList()
+        var nStr = n.toString()
         println(nStr)
         for (i in nStr) {
             println(i.digitToInt())
             answer += i.digitToInt()
+            println(answer)
         }
         return answer
     }
