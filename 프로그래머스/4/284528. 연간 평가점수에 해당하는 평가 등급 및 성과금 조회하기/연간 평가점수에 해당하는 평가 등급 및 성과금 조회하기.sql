663458c8a28e6f64f4932e04941c986e9df53fe1
miso1105 <137920347+miso1105@users.noreply.github.com>
2024-06-10 20:23:11 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,19 @@
+-- 코드를 작성해주세요
+WITH TEMP AS 
+(
+    SELECT EMP_NO,
+        CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+            WHEN AVG(SCORE) >= 90 THEN 'A'
+            WHEN AVG(SCORE) >= 80 THEN 'B'
+            ELSE 'C' END AS GRADE
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+)
+SELECT E.EMP_NO, E.EMP_NAME, T.GRADE, 
+    CASE WHEN T.GRADE = 'S' THEN (E.SAL * 0.2)
+        WHEN T.GRADE = 'A' THEN (E.SAL * 0.15)
+        WHEN T.GRADE = 'B' THEN (E.SAL * 0.1)
+        ELSE E.SAL * 0 END AS BONUS
+FROM TEMP T
+    LEFT JOIN HR_EMPLOYEES E
+ON T.EMP_NO = E.EMP_NO
